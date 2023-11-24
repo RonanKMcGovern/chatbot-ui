4f5b375727f8ef0d3b4ef2e2bc6c7692da3848dc
RonanKMcGovern <78278410+RonanKMcGovern@users.noreply.github.com>
2023-11-24 16:45:35 +0000

    clean up fonts

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -352,10 +352,10 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to chat.Trelis.com - a hosted version of Chatbot UI
+            Welcome to Chat.Trelis.com
           </div>
           <div className="text-center text-lg text-black dark:text-white">
-            <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
+            <div className="mb-8">{`Chat.Trelis.com is a hosted version of Chatbot UI - an open source clone of OpenAI's ChatGPT UI.`}</div>
             <div className="mb-2 font-bold">
               Important: chat.Trelis.com and Chatbot UI are 100% unaffiliated with OpenAI.
             </div>
